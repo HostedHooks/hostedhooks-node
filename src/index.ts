85536634677af4e819b9fbe18fc3c0ac28c9c8b3
Salim Absi <salimabsifull@gmail.com>
2022-09-16 09:22:58 +0300

    Add pagination query option

--- src/index.ts
+++ src/index.ts
@@ -62,6 +62,22 @@ type CreateMessageParams = {
 	override_payload?: boolean
 }
 
+type PaginationQuery = Partial<{
+	page: number
+	per_page: number
+	offset: number
+}>
+
+function serializeQuery(obj: { [p: string]: string | number }) {
+	const str = []
+	for (let p in obj) {
+		if (obj.hasOwnProperty(p)) {
+			str.push(`${encodeURIComponent(p)}=${encodeURIComponent(obj[p])}`)
+		}
+	}
+	return `?${str.join("&")}`
+}
+
 export class HostedHooks {
 	private baseUrl = "https://hostedhooks.com/api/v1"
 	private apiKey: string
@@ -70,9 +86,13 @@ export class HostedHooks {
 		this.apiKey = (apiKey ?? process.env.HOSTEDHOOKS_API_KEY) as string
 	}
 
-	private async get<T>(url: string): Promise<T> {
+	private async get<T>(url: string, query?: PaginationQuery): Promise<T> {
 		try {
-			const response = await fetch(`${this.baseUrl}${url}`, {
+			let queryString = ""
+			if (query) {
+				queryString = serializeQuery(query)
+			}
+			const response = await fetch(`${this.baseUrl}${url}${queryString}`, {
 				method: "GET",
 				headers: { Authorization: `Bearer ${this.apiKey}` },
 			})
@@ -137,8 +157,8 @@ export class HostedHooks {
 	}
 
 	// Apps
-	public async list_apps() {
-		return this.get<App[]>("/apps")
+	public async list_apps(query?: PaginationQuery) {
+		return this.get<App[]>("/apps", query)
 	}
 	public async create_app(params: { name: string }) {
 		return this.post<App>("/apps", params)
@@ -148,22 +168,22 @@ export class HostedHooks {
 	}
 
 	// Subscriptions
-	public async get_subscription(uuid: string) {
-		return this.get<Subscription>(`/subscriptions${uuid}`)
+	public async get_subscription(uuid: string, query?: PaginationQuery) {
+		return this.get<Subscription>(`/subscriptions${uuid}`, query)
 	}
-	public async list_subscriptions(app_uuid: string) {
-		return this.get<Subscription[]>(`/apps/${app_uuid}/subscriptions`)
+	public async list_subscriptions(app_uuid: string, query?: PaginationQuery) {
+		return this.get<Subscription[]>(`/apps/${app_uuid}/subscriptions`, query)
 	}
 	public async create_subscription(app_uuid: string, params: { name: string }) {
 		return this.post<Subscription>(`/apps/${app_uuid}/subscriptions`, params)
 	}
 
 	// Endpoints
-	public async get_endpoint(app_uuid: string, endpoint_uuid: string) {
-		return this.get<Endpoint>(`/apps/${app_uuid}/endpoints/${endpoint_uuid}`)
+	public async get_endpoint(app_uuid: string, endpoint_uuid: string, query?: PaginationQuery) {
+		return this.get<Endpoint>(`/apps/${app_uuid}/endpoints/${endpoint_uuid}`, query)
 	}
-	public async list_endpoints(app_uuid: string) {
-		return this.get<Endpoint[]>(`/apps/${app_uuid}/endpoints`)
+	public async list_endpoints(app_uuid: string, query?: PaginationQuery) {
+		return this.get<Endpoint[]>(`/apps/${app_uuid}/endpoints`, query)
 	}
 	public async create_endpoint(subscription_uuid: string, params: CreateEndpointParams) {
 		return this.post<Endpoint>(`/subscriptions/${subscription_uuid}/endpoints`, params)
@@ -180,8 +200,8 @@ export class HostedHooks {
 	}
 
 	// Webhook Events
-	public async list_webhook_events(app_uuid: string) {
-		return this.get<WebhookEvent[]>(`/apps/${app_uuid}/webhook_events`)
+	public async list_webhook_events(app_uuid: string, query?: PaginationQuery) {
+		return this.get<WebhookEvent[]>(`/apps/${app_uuid}/webhook_events`, query)
 	}
 
 	// Messages
